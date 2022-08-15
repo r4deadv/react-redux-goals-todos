f0f849cfdc96fb99c4d5cf6618e9c95130498456
r4deadv <mihovilovic.rade@gmail.com>
2022-08-15 15:18:54 +0200

    create store function added

--- src/index.js
+++ src/index.js
@@ -12,13 +12,33 @@ function createStore() {
   // 4. Update the state
 
   let state;
+  let listeners = [];
 
   const getState = () => state;
 
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
   return {
     getState,
+    subscribe,
   };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
+});
+
+const unsubscribe = store.subscribe(() => {
+  console.log("The store changed.");
+});
+
+unsubscribe();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
