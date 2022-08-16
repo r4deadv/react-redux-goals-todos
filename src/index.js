283e355a45508a9154b53aea427562cbfe484667
r4deadv <mihovilovic.rade@gmail.com>
2022-08-16 11:59:36 +0200

    goals reducer function added

--- src/index.js
+++ src/index.js
@@ -37,18 +37,54 @@ function createStore(reducer) {
 }
 
 // App Code
+// reducer function
 function todos(state = [], action) {
-  if (action.type === "ADD_TODO") {
-    return state.concat([action.todo]);
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter((todo) => todo.id !== action.id);
+    case "TOGGLE_TODO":
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
   }
-
-  return state;
 }
 
-// const store = createStore(todos);
-// store.subscribe(() => {
-//   console.log("The new state is: ", store.getState());
-// });
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter((goal) => goal.id !== action.id);
+    case "TOGGLE_GOAL":
+      return state.map((goal) =>
+        goal.id !== action.id
+          ? goal
+          : Object.assign({}, goal, { complete: !goal.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+const store = createStore(todos);
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false,
+  },
+});
 
 // const unsubscribe = store.subscribe(() => {
 //   console.log("The store changed.");
