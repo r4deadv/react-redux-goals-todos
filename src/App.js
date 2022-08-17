6f9532c8d5a1f7e9fb94cdb80c702dbb740e0da9
r4deadv <mihovilovic.rade@gmail.com>
2022-08-17 19:10:37 +0200

    adding in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>React App</p>
+    </div>
+  );
 }
 
 export default App;
