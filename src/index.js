c6f7d3c55e0d534f3e370d53daa84f0fea958942
r4deadv <mihovilovic.rade@gmail.com>
2022-08-25 14:29:40 +0200

    Utilizing Context on App, Goals and Todos components

--- src/index.js
+++ src/index.js
@@ -1,190 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Library Code
-function createStore(reducer) {
-  // The store should have four parts
-  // 1. The state
-  // 2. Get the state
-  // 3. Listen to changes on the state
-  // 4. Update the state
+const Context = React.createContext();
 
-  let state;
-  let listeners = [];
+const Parent = () => {
+  return (
+    <div>
+      <h1>Parent</h1>
+      <Child />
+    </div>
+  );
+};
 
-  const getState = () => state;
+const Child = () => {
+  return (
+    <div>
+      <h1>Child</h1>
+      <Grandchild />
+    </div>
+  );
+};
 
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter((l) => l !== listener);
-    };
-  };
+const Grandchild = ({ name }) => {
+  return (
+    <Context.Consumer>
+      {(name) => (
+        <div>
+          <h1>Grandchild</h1>
+          <h3>Name: {name}</h3>
+        </div>
+      )}
+    </Context.Consumer>
+  );
+};
 
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach((listener) => listener());
-  };
+const App = () => {
+  const name = "Andrew";
 
-  return {
-    getState,
-    subscribe,
-    dispatch,
-  };
-}
-
-// App Code
-const ADD_TODO = "ADD_TODO";
-const REMOVE_TODO = "REMOVE_TODO";
-const TOGGLE_TODO = "TOGGLE_TODO";
-const ADD_GOAL = "ADD_GOAL";
-const REMOVE_GOAL = "REMOVE_GOAL";
-const TOGGLE_GOAL = "TOGGLE_GOAL";
-
-// action creators
-function addTodoAction(todo) {
-  return {
-    type: ADD_TODO,
-    todo,
-  };
-}
-
-function removeTodoAction(id) {
-  return {
-    type: REMOVE_TODO,
-    id,
-  };
-}
-
-function toggleTodoAction(id) {
-  return {
-    type: TOGGLE_TODO,
-    id,
-  };
-}
-
-function addGoalAction(goal) {
-  return {
-    type: ADD_GOAL,
-    goal,
-  };
-}
-
-function removeGoalAction(id) {
-  return {
-    type: REMOVE_GOAL,
-    id,
-  };
-}
-
-function toggleGoalAction(id) {
-  return {
-    type: TOGGLE_GOAL,
-    id,
-  };
-}
-
-// reducer function
-function todos(state = [], action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat([action.todo]);
-    case REMOVE_TODO:
-      return state.filter((todo) => todo.id !== action.id);
-    case TOGGLE_TODO:
-      return state.map((todo) =>
-        todo.id !== action.id
-          ? todo
-          : Object.assign({}, todo, { complete: !todo.complete })
-      );
-    default:
-      return state;
-  }
-}
-
-// reducer function
-function goals(state = [], action) {
-  switch (action.type) {
-    case ADD_GOAL:
-      return state.concat([action.goal]);
-    case REMOVE_GOAL:
-      return state.filter((goal) => goal.id !== action.id);
-    case TOGGLE_GOAL:
-      return state.map((goal) =>
-        goal.id !== action.id
-          ? goal
-          : Object.assign({}, goal, { complete: !goal.complete })
-      );
-    default:
-      return state;
-  }
-}
-
-// root reducer -> combine reducers
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action),
-  };
-}
-
-const store = createStore(app);
-store.subscribe(() => {
-  console.log("The new state is: ", store.getState());
-});
-
-store.dispatch(
-  addTodoAction({
-    id: 0,
-    name: "Learn Redux",
-    complete: false,
-  })
-);
-
-store.dispatch(
-  addTodoAction({
-    id: 1,
-    name: "Wash the car",
-    complete: false,
-  })
-);
-
-store.dispatch(
-  addTodoAction({
-    id: 2,
-    name: "Go to the gym",
-    complete: true,
-  })
-);
-
-store.dispatch(removeTodoAction(1));
-
-store.dispatch(toggleTodoAction(0));
-
-store.dispatch(
-  addGoalAction({
-    id: 0,
-    name: "Learn Redux",
-  })
-);
-
-store.dispatch(
-  addGoalAction({
-    id: 1,
-    name: "Lose 20 pounds",
-  })
-);
-
-store.dispatch(removeGoalAction(0));
-
-// const unsubscribe = store.subscribe(() => {
-//   console.log("The store changed.");
-// });
-
-// unsubscribe();
+  return (
+    <Context.Provider value={name}>
+      <Parent />
+    </Context.Provider>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
